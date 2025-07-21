566443eecb11ee7aec481a5e6f752fab7a21541d
Conqueror555 <158792363+Conqueror555@users.noreply.github.com>
2025-07-21 22:26:39 +0530

    final

--- client/app/pending/page.tsx
+++ client/app/pending/page.tsx
@@ -40,8 +40,8 @@ export default function Home() {
       { filtered.map((task: Task,i: number) => (
           <TaskItem key={i} task={task}/>
         ))}
-        <motion.button className="h-[16rem] w-full py-2 rounded-md text-lg font-medium text-gray-500 border-dashed border-2 border-gray-400
-        hover:bg-gray-300 hover:border-none transition duration-200 ease-in-out"
+        <motion.button className="h-[16rem] w-full py-2 rounded-md text-lg font-medium text-black border-dashed border-2 border-gray-600
+        hover:bg-gray-500 hover:border-none transition duration-200 ease-in-out"
         onClick={ openModalForAdd}
         variants={item}>
           Add New Task
