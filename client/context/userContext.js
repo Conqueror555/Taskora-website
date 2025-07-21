bd338f3475aba9acc8c83786c0a5977f613da9f4
Conqueror555 <158792363+Conqueror555@users.noreply.github.com>
2025-07-21 22:02:36 +0530

    final1

--- client/context/userContext.js
+++ client/context/userContext.js
@@ -11,7 +11,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl ="http://localhost:8000";
+  const serverUrl ="https://taskora-website.onrender.com";
 
   const router = useRouter();
   const pathname = usePathname(); 
